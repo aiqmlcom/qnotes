563231dc9ead35d19e897356f20d88d06bbcadd6
aiqml <aiqmlcom@gmail.com>
2022-12-16 07:47:09 +0530

    Email verification

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -30,65 +30,50 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text('Login')),
-        body: FutureBuilder(
-          future: Firebase.initializeApp(
-              options: DefaultFirebaseOptions.currentPlatform),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.done:
-                return Column(
-                  children: [
-                    TextField(
-                      controller: _email,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                          hintText: 'Enter your email here'),
-                    ),
-                    TextField(
-                      controller: _password,
-                      obscureText: true,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      decoration: const InputDecoration(
-                          hintText: 'Enter your password here'),
-                    ),
-                    TextButton(
-                        onPressed: () async {
-                          final email = _email.text;
-                          final password = _password.text;
+    return Column(
+      children: [
+        TextField(
+          controller: _email,
+          enableSuggestions: false,
+          autocorrect: false,
+          keyboardType: TextInputType.emailAddress,
+          decoration: const InputDecoration(hintText: 'Enter your email here'),
+        ),
+        TextField(
+          controller: _password,
+          obscureText: true,
+          enableSuggestions: false,
+          autocorrect: false,
+          decoration:
+              const InputDecoration(hintText: 'Enter your password here'),
+        ),
+        TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
 
-                          try {
-                            final userCredential = await FirebaseAuth.instance
-                                .signInWithEmailAndPassword(
-                                    email: email, password: password);
-                            print(userCredential);
-                          } on FirebaseAuthException catch (e) {
-                            switch (e.code) {
-                              case 'user-not-found':
-                                print('User not found');
-                                break;
-                              case 'wrong-password':
-                                print('Wrong password');
-                                break;
-                              default:
-                                print(e.code);
-                            }
-                          } catch (e) {
-                            print(e);
-                          }
-                        },
-                        child: const Text('Login')),
-                  ],
-                );
-
-              default:
-                return const Text('Loading...');
-            }
-          },
-        ));
+              try {
+                final userCredential = await FirebaseAuth.instance
+                    .signInWithEmailAndPassword(
+                        email: email, password: password);
+                print(userCredential);
+              } on FirebaseAuthException catch (e) {
+                switch (e.code) {
+                  case 'user-not-found':
+                    print('User not found');
+                    break;
+                  case 'wrong-password':
+                    print('Wrong password');
+                    break;
+                  default:
+                    print(e.code);
+                }
+              } catch (e) {
+                print(e);
+              }
+            },
+            child: const Text('Login')),
+      ],
+    );
   }
 }
