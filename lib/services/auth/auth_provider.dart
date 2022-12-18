05369bb6bc0dd1256d5764ae5f3414092e983fce
aiqml <aiqmlcom@gmail.com>
2022-12-18 11:47:05 +0530

    Firebase Auth Provider ready

--- lib/services/auth/auth_provider.dart
+++ lib/services/auth/auth_provider.dart
@@ -2,8 +2,14 @@ import 'package:qnotes/services/auth/auth_user.dart';
 
 abstract class AuthProvider {
   AuthUser? get currentUser;
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
   Future<AuthUser> logIn({
     required String email,
     required String password,
   });
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
 }
