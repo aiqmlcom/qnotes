054e1405bf15d30ae0ba2e45d935fe61ab475d99
aiqml <aiqmlcom@gmail.com>
2022-12-19 11:03:35 +0530

    Connecting to the notes service

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:qnotes/constants/routes.dart';
 import 'package:qnotes/services/auth/auth_service.dart';
+import 'package:qnotes/services/crud/notes_service.dart';
 
 import '../enums/menu_action.dart';
 
@@ -12,6 +13,22 @@ class NotesView extends StatefulWidget {
 }
 
 class _NotesViewState extends State<NotesView> {
+  late final NotesService _notesService;
+  String get userEmail => AuthService.firebase().currentUser!.email!;
+
+  @override
+  void initState() {
+    _notesService = NotesService();
+    _notesService.open();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _notesService.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
