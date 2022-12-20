8834d975ff2373faaafa2d18c8f00bd07eb45a0e
aiqml <aiqmlcom@gmail.com>
2022-12-20 09:13:46 +0530

    Firebase Cloud Storage Class

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:qnotes/services/cloud/cloud_note.dart';
 import 'package:qnotes/services/cloud/cloud_storage_constants.dart';
+import 'package:qnotes/services/cloud/cloud_storage_exceptions.dart';
 
 class FirebaseCloudStorage {
   final notes = FirebaseFirestore.instance.collection('notes');
@@ -14,5 +16,52 @@ class FirebaseCloudStorage {
       ownerUserIdFieldName: ownerUserId,
       textFieldName: '',
     });
+  }
+
+  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
+    try {
+      return await notes
+          .where(
+            ownerUserIdFieldName,
+            isEqualTo: ownerUserId,
+          )
+          .get()
+          .then(
+            (value) => value.docs.map((doc) {
+              return CloudNote(
+                documentId: doc.id,
+                ownerUserId: doc.data()[ownerUserIdFieldName] as String,
+                text: doc.data()[textFieldName] as String,
+              );
+            }),
+          );
+    } catch (e) {
+      throw CouldNotGetAllNotesException();
+    }
+  }
+
+  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) {
+    return notes.snapshots().map((event) => event.docs
+        .map((doc) => CloudNote.fromSnapshot(doc))
+        .where((note) => note.ownerUserId == ownerUserId));
+  }
+
+  Future<void> updateNote({
+    required String documentId,
+    required String text,
+  }) async {
+    try {
+      await notes.doc(documentId).update({textFieldName: text});
+    } catch (e) {
+      throw CouldNotUpdateNoteException();
+    }
+  }
+
+  Future<void> deleteNote({required String documentId}) async {
+    try {
+      await notes.doc(documentId).delete();
+    } catch (e) {
+      throw CouldNotDeleteNoteException();
+    }
   }
 }
