037fb13b58fe951ca28865f6fd5bc8f226209e67
aiqml <aiqmlcom@gmail.com>
2022-12-21 23:17:21 +0530

    Using Auth Bloc

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:qnotes/constants/routes.dart';
 import 'package:qnotes/services/auth/auth_exceptions.dart';
-import 'package:qnotes/services/auth/auth_service.dart';
+import 'package:qnotes/services/auth/bloc/auth_bloc.dart';
+import 'package:qnotes/services/auth/bloc/auth_event.dart';
 import 'package:qnotes/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -57,24 +59,7 @@ class _LoginViewState extends State<LoginView> {
                 final password = _password.text;
 
                 try {
-                  await AuthService.firebase().logIn(
-                    email: email,
-                    password: password,
-                  );
-                  if (AuthService.firebase().currentUser?.isEmailVerified ??
-                      false) {
-                    if (!mounted) return;
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      notesRoute,
-                      (route) => false,
-                    );
-                  } else {
-                    if (!mounted) return;
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      verifyEmailRoute,
-                      (route) => false,
-                    );
-                  }
+                  context.read<AuthBloc>().add(AuthEventLogIn(email, password));
                 } on UserNotFoundAuthException {
                   await showErrorDialog(
                     context,
