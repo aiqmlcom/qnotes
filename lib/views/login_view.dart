de796b2b0b097d513d4b6935184923df61854e9d
aiqml <aiqmlcom@gmail.com>
2022-12-22 07:05:05 +0530

    Login view exception handling

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -4,6 +4,7 @@ import 'package:qnotes/constants/routes.dart';
 import 'package:qnotes/services/auth/auth_exceptions.dart';
 import 'package:qnotes/services/auth/bloc/auth_bloc.dart';
 import 'package:qnotes/services/auth/bloc/auth_event.dart';
+import 'package:qnotes/services/auth/bloc/auth_state.dart';
 import 'package:qnotes/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -53,31 +54,41 @@ class _LoginViewState extends State<LoginView> {
             decoration:
                 const InputDecoration(hintText: 'Enter your password here'),
           ),
-          TextButton(
-              onPressed: () async {
-                final email = _email.text;
-                final password = _password.text;
-
-                try {
-                  context.read<AuthBloc>().add(AuthEventLogIn(email, password));
-                } on UserNotFoundAuthException {
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) async {
+              if (state is AuthStateLoggedOut) {
+                if (state.exception is UserNotFoundAuthException) {
                   await showErrorDialog(
                     context,
                     'User not found',
                   );
-                } on WrongPasswordAuthException {
+                } else if (state.exception is WrongPasswordAuthException) {
                   await showErrorDialog(
                     context,
                     'Wrong credentials',
                   );
-                } on GenericAuthException {
+                } else if (state.exception is GenericAuthException) {
                   await showErrorDialog(
                     context,
                     'Authentication Error',
                   );
                 }
-              },
-              child: const Text('Login')),
+              }
+            },
+            child: TextButton(
+                onPressed: () async {
+                  final email = _email.text;
+                  final password = _password.text;
+
+                  context.read<AuthBloc>().add(
+                        AuthEventLogIn(
+                          email,
+                          password,
+                        ),
+                      );
+                },
+                child: const Text('Login')),
+          ),
           TextButton(
               onPressed: () async {
                 await Navigator.of(context)
